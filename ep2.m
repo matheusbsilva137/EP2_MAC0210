f124567c947313e737500bcc799e216920f9ebeb
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2020-06-11 15:34:34 -0300

    V3

--- ep2.m
+++ ep2.m
@@ -6,21 +6,21 @@ originalImg = imread(imgFileName);
 compressedImg = compress(originalImg, k);
 decompressedImg = decompress(compressedImg, method, k, k+2);
 
-imgM = zeros(1498, 1498);
-for a = 1 : 3
-    for x = 1 : 1498
-       for y = 1 : 1498
-           if a == 1
-               imgM(x,y, a) = sin(x);
-           elseif a == 2
-               imgM(x,y, a) = (sin(x)+sin(y))/2;
-           else
-               imgM(x,y, a) = sin(x);
-           end
-       end
-    end
-end
-
-imwrite(imgM, 'plot.png', 'png');
+% imgM = zeros(1498, 1498);
+% for a = 1 : 3
+%     for x = 1 : 1498
+%        for y = 1 : 1498
+%            if a == 1
+%                imgM(x,y, a) = sin(x);
+%            elseif a == 2
+%                imgM(x,y, a) = (sin(x)+sin(y))/2;
+%            else
+%                imgM(x,y, a) = sin(x);
+%            end
+%        end
+%     end
+% end
+% 
+% imwrite(imgM, 'plot.png', 'png');
 err = calculateError(originalImg, decompressedImg);
 fprintf('Erro: %f\n', err);
