fee249cc04bd1e48d4f6ce0ce45797da8216a77f
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2020-06-11 13:30:19 -0300

    V2

--- ep2.m
+++ ep2.m
@@ -1,19 +1,21 @@
 clear variables;
 imgFileName = input('Informe o nome da imagem: ', 's');
+method = input('Informe o método de descompressão da imagem (1- Bilinear, 2 - Bicúbico): ');
+k = input('Informe o valor de k: ');
 originalImg = imread(imgFileName);
-compressedImg = compress(originalImg, 2);
-decompressedImg = decompress(compressedImg, 2, 2, 4);
+compressedImg = compress(originalImg, k);
+decompressedImg = decompress(compressedImg, method, k, k+2);
 
 imgM = zeros(1498, 1498);
 for a = 1 : 3
     for x = 1 : 1498
        for y = 1 : 1498
            if a == 1
-               imgM(x,y, a) = sin(1/x);
+               imgM(x,y, a) = sin(x);
            elseif a == 2
-               imgM(x,y, a) = 0;
+               imgM(x,y, a) = (sin(x)+sin(y))/2;
            else
-               imgM(x,y, a) = sin(1/x);
+               imgM(x,y, a) = sin(x);
            end
        end
     end
